19b0d08cfee2120fdabc56045a6ff532f6b5d147
Vanessabellegarde <vanessa.bellegarde@outlook.com>
2022-04-08 11:43:06 -0400

    Creation de CoupeDuMonde et Equipe

--- cup/src/main/java/com/football/api/resources/import.sql
+++ cup/src/main/java/com/football/api/resources/import.sql
@@ -5,3 +5,23 @@
 
 begin transaction;
 
+drop schema if exists therrijo cascade;
+create schema therrijo;
+set search_path to therrijo;
+
+CREATE TABLE CoupeDuMonde (
+    annee int PRIMARY KEY,
+    Pays_Coupe text,
+    Start_Date int,
+    End_date int
+); 
+
+CREATE TABLE Equipe (
+    ID_Equipe int PRIMARY KEY,
+    Annee int,
+    ADD FOREIGN KEY (Annee) REFERENCES CoupeDuMonde(annee)
+    Pays text,
+); 
+
+commit;
+rollback;
