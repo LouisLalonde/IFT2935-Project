1b9c450cc61f1947fb2791faaa0205769d66ce2a
LouisLalonde <louis.lalonde@umontreal.ca>
2022-04-08 19:50:19 -0400

    Ui update

--- cup/src/main/java/com/football/App.java
+++ cup/src/main/java/com/football/App.java
@@ -1,8 +1,4 @@
 package com.football;
-import java.util.ArrayList;
-import java.util.HashMap;
-import com.football.api.services.DataBase.Operation;
-import com.football.webapp.WEB_INF.classes.Student;
 import com.football.webapp.WEB_INF.services.UserService;
 import com.football.webapp.WEB_INF.views.UI;
 
@@ -30,10 +26,6 @@ public class App {
         backEnd(userService);
         // Starting the front-end
         frontEnd(args);
-        // Testing the creation of a student in the data base 
-        // userService.appUseServices().appUseDataBase().dataBaseService().executeQuery(Operation.CREATE, new HashMap<String, String>() {{
-        //     put("", "");
-        // }});
     }
 }
 
