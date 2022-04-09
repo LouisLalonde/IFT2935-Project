0c8147475418e12aa46ec11385d506813a069a79
LouisLalonde <louis.lalonde@umontreal.ca>
2022-04-09 12:54:08 -0400

    Added new import.sql file

--- cup/src/main/java/com/football/api/resources/import.sql
+++ cup/src/main/java/com/football/api/resources/import.sql
@@ -3,7 +3,6 @@ BEGIN;
 drop schema if exists football cascade;
 create schema football;
 
-<<<<<<< HEAD
 drop schema if exists football cascade;
 create schema football;
 set search_path to football;
@@ -141,12 +140,3 @@ INSERT INTO CoupeDuMonde VALUES (1978, "Argentina", 1978-06-01, 1978-06-25);
 
 commit;
 rollback;
-=======
-drop table if exists student;
-
-CREATE TABLE student (id INTEGER,txt_firstname VARCHAR(255),txt_lastname VARCHAR(255),num_roll INTEGER,txt_standard VARCHAR(255));
-INSERT INTO student VALUES (12, 'David', 'Lalonde', 999, 'pgpsql');
-INSERT INTO student VALUES (12, 'Louis', 'Lalonde', 888, 'pgpsql');
-
-COMMIT;
->>>>>>> main
