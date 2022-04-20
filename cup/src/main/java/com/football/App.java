43002844dbc9a493548f860ba17f6eec255764bc
LouisLalonde <louis.lalonde@umontreal.ca>
2022-04-20 11:45:36 -0400

    Updated ui.

--- cup/src/main/java/com/football/App.java
+++ cup/src/main/java/com/football/App.java
@@ -1,8 +1,12 @@
 package com.football;
 import com.football.webapp.WEB_INF.services.UserService;
+<<<<<<< Updated upstream
 import com.football.webapp.WEB_INF.views.Home;
 import org.hibernate.Session;
 
+=======
+import com.football.webapp.WEB_INF.views.UI;
+>>>>>>> Stashed changes
 /**
  * Main class for the App.
  */
@@ -24,9 +28,30 @@ public class App {
         // Data
         // Logic
         // Starting the back-end API
+<<<<<<< Updated upstream
         Session session = backEnd(userService);
         // Starting the front-end
         frontEnd(args, session);
+=======
+        backEnd(userService);
+        UI.main(args);
+        // Testing the creation of a student in the data base 
+        // userService.appUseServices().appUseDataBase().dataBaseService().executeQuery(Operation.CREATE, new HashMap<String, String>() {{
+        //     put("", "");
+        // }});
+        // // Testing the read of the student table
+        // HashMap<String, Object> res =  userService.appUseServices().appUseDataBase().dataBaseService().executeQuery(
+        //     Operation.READ, new HashMap<String, String>() {{
+        //         put("queryNumber", "1");
+        //         put("mapCalss", "com.football.webapp.WEB_INF.classes.Student");
+        //     }});
+        // System.out.println("Sdtout ---");
+        // String jsonObjects = (String) res.get("body");
+        // System.out.println(jsonObjects);
+        // // Converting the json objects to the given class
+        // ArrayList<Student> students = userService.appUseServices().appUseDataBase().dataBindService().DataBindModel().deserialize(jsonObjects, Student.class);
+        // System.out.println(students);
+>>>>>>> Stashed changes
     }
 }
 
--- cup/src/main/java/com/football/api/resources/import.sql
+++ cup/src/main/java/com/football/api/resources/import.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+<<<<<<< Updated upstream
 drop schema if exists football cascade;
 create schema football;
 
@@ -350,4 +351,15 @@ INSERT INTO Sanction VALUES (11, 99, 3, 10, '01:08:41', 2);
 
 
 commit;
+=======
+drop DATABASE if exists football cascade;
+create DATABASE football OWNER postgres;
+>>>>>>> Stashed changes
 
+drop table if exists student;
+
+CREATE TABLE student (id INTEGER,txt_firstname VARCHAR(255),txt_lastname VARCHAR(255),num_roll INTEGER,txt_standard VARCHAR(255));
+INSERT INTO student VALUES (12, 'David', 'Lalonde', 999, 'pgpsql');
+INSERT INTO student VALUES (12, 'Louis', 'Lalonde', 888, 'pgpsql');
+
+COMMIT;
